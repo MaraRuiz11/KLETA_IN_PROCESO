0d16989a93ab9211cad5c9aaa32d1eea8301b882
MaraRuiz11 <marapanduro35@gmail.com>
2026-04-14 22:24:51 -0500

    Add files via upload

---
+++ backend/src/test/java/com/senati/Restaurante/Jugeria/KLETA/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senati.Restaurante.Jugeria.KLETA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
