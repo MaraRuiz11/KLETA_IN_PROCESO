d12bb14ba7a977c7b4bc475ad45affb40b61aa47
MaraRuiz11 <marapanduro35@gmail.com>
2026-04-20 16:46:46 -0500

    por subir

--- backend/src/main/java/com/senati/RestauranteKleta/repository/DetalleVentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senati.RestauranteKleta.repository;
-
-import com.senati.RestauranteKleta.entity.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-}
